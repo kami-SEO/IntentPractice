146fa96f3942a21a125b0434825b0124afefc211
SKM <rkdalsv@naver.com>
2023-12-23 22:15:07 +0900

    다른 화면 추가 UI 생성 재생성

--- app/src/main/java/com/example/intentpractice/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice/OtherActivity.kt
@@ -1,22 +1,14 @@
 package com.example.intentpractice
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.intentpractice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class OtherActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.moveToOtherBtn.setOnClickListener {
-
-            //다른화면으로 이동(otherActivity)
-            val myIntent = Intent(this,otherActivity::class.java)
-            startActivity(myIntent)
-        }
     }
 }

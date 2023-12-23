7d11ee775e51097a456c67d5c8107ae2627d057c
SKM <rkdalsv@naver.com>
2023-12-23 17:20:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.intentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intentpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
